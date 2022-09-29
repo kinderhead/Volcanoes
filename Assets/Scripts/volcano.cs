b9faff55ec10f8e07dcb1d15f1931994553f38b2
kinderhead <kinderhead512@gmail.com>
2022-09-29 16:23:50 -0400

    VOLCANO

--- Assets/Scripts/volcano.cs
+++ Assets/Scripts/volcano.cs
@@ -3,9 +3,12 @@ using System;
 
 public class volcano : MeshInstance
 {
+    [Export]
+    public Spatial Crater;
+
     public override void _Ready()
     {
-        
+        Crater = GetNode<Spatial>("Crater");
     }
 
     public override void _Process(float delta)
