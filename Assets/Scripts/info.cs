604189d60350cd812fcfb54a77c6a84d852e06ad
kinderhead <kinderhead512@gmail.com>
2022-10-03 19:14:36 -0400

    nice

--- Assets/Scripts/info.cs
+++ Assets/Scripts/info.cs
@@ -8,9 +8,9 @@ public class info : Label
 
     [Export]
     public string[] Boxes = {
-        "Cindercone:\nTesting",
-        "Stratovolcano:",
-        "Shield volcano"
+        "Cinder cone:\nCinder cones are a special type of volcano formed from ash from mafic volcanic eruptions. They sometimes can grow thousands of feet. Their slope is around 30 degrees. Cinder cones frequently form near other, larger volcanoes.\nDangers include: Lava flows, ash, and lava bombs.",
+        "Stratovolcano:\nStratovolcanoes or composite volcanoes are large volcano made of a variety of rock types like basalt and rhyolite. They are what you typically think of when you think about volcanoes. They grow very slowly from lava and pyroclastic flows. They typically form near plate boundaries, hotspots, and other geological activites.\nDangers include: Lava flows, pyroclastic flows, ash.",
+        "Shield volcano:\nShield volcanoes are the biggest type of volcano. They are typically made of basalt, or similar. They take a very long time to grow. The tallest mountain on Earth, Mauna Kea, is a shield volcano. Shield volcanoes form near plate boundaries and hotspots.\nDangers include: Lava flows, lava bombs, explosions."
     };
 
     public override void _Ready()
@@ -20,6 +20,26 @@ public class info : Label
 
     public void Display() {
         Text = Boxes[Info];
+    }
+
+    public override void _Input(InputEvent @event) {
+        if (@event.IsActionPressed("info_left")) {
+            if (Info == 0) {
+                Info = Boxes.Length - 1;
+            }
+            else {
+                Info--;
+            }
+        }
+
+        if (@event.IsActionPressed("info_right")) {
+            Info++;
+            if (Info == Boxes.Length) {
+                Info = 0;
+            }
+        }
+
+        Display();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
--- README.md
+++ README.md
@@ -8,3 +8,17 @@ A simple simulation of a volcanoes.
 * Cinder cone because they are cool
 * Simulated lava flows
 * Information about volcanos and the damage they can cause
+
+## Annotated Bibliography
+
+Britannica, The Editors of Encyclopaedia. "cinder cone". Encyclopedia Britannica, 16 Feb. 2016, https://www.britannica.com/science/cinder-cone.
+
+I used this source as information on the model and eruption simulation. It was also used for the info box on cinder cones.
+
+Britannica, The Editors of Encyclopaedia. "Stratovolcanoes". Encyclopedia Britannica, 11 Feb. 2022, https://www.britannica.com/science/volcano/Stratovolcanoes.
+
+This source was used as information for stratovolcanoes.
+
+“Shield Volcanoes.” Shield Volcanoes (U.S. National Park Service), National Park Service, 30 Sept. 2022, https://www.nps.gov/articles/000/shield-volcanoes.htm.
+
+This source was used as information for shield volcanoes.
