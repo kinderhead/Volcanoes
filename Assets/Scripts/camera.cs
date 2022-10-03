592331d1174db7565628dd82984a5ea9daacd56f
kinderhead <kinderhead512@gmail.com>
2022-10-03 12:32:05 -0400

    eruption time

--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -5,29 +5,25 @@ public class camera : Camera
 {
     public override void _Ready()
     {
-        
+
     }
 
     public override void _Process(float delta)
     {
         if (Input.IsActionPressed("ui_left")) {
-            GD.Print("ug");
             GetParent<Spatial>().Rotate(Vector3.Up, -0.01f);
         }
 
         if (Input.IsActionPressed("ui_right")) {
-            GD.Print("ug");
             GetParent<Spatial>().Rotate(Vector3.Up, 0.01f);
         }
 
         if (Input.IsActionPressed("ui_up")) {
-            GD.Print("ug");
-            GetParent<Spatial>().Rotate(Vector3.Right, -0.01f);
+            GetParent<Spatial>().RotateObjectLocal(Vector3.Right, -0.01f);
         }
 
         if (Input.IsActionPressed("ui_down")) {
-            GD.Print("ug");
-            GetParent<Spatial>().Rotate(Vector3.Right, 0.01f);
+            GetParent<Spatial>().RotateObjectLocal(Vector3.Right, 0.01f);
         }
     }
 }
--- Assets/Scripts/volcano.cs
+++ Assets/Scripts/volcano.cs
@@ -6,13 +6,42 @@ public class volcano : MeshInstance
     [Export]
     public Spatial Crater;
 
+    [Export]
+    public Control UI;
+
+    [Export]
+    public Spatial Lavabombs;
+
+    public static bool Erupting {get; private set;} = false;
+
     public override void _Ready()
     {
         Crater = GetNode<Spatial>("Crater");
+        UI = GetParent().GetNode<Control>("Control");
+
+        GD.Randomize();
     }
 
     public override void _Process(float delta)
     {
-        
+        if (UI.GetNode<Button>("Erupt").Pressed && !Erupting) {
+            Erupt();
+        }
+
+        if (GD.RandRange(0, 100) > 98) {
+            GD.Print("Boo");
+        }
+    }
+
+    public async void Erupt() {
+        GD.Print("Erupting");
+        Erupting = true;
+        Crater.GetNode<Particles>("Particles").Emitting = true;
+
+        await ToSignal(GetTree().CreateTimer(new Random().Next(15, 60)), "timeout");
+
+        Crater.GetNode<Particles>("Particles").Emitting = false;
+        Erupting = false;
+        GD.Print("Done erupting");
     }
 }
